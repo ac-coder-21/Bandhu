6f1204c234e465891c2c5411a5709d4a78301fbc
Amogh Chavan <auuchavan@gmail.com>
2024-03-20 17:47:26 +0530

    anxiety test integration

--- app.py
+++ app.py
@@ -3,8 +3,9 @@ from functools import wraps
 import pymongo
 from user.models import User
 
-from GT.chat import get_response
-from GT.det_que_or_continuation import gmh_questions
+from GT.chat_gt import get_response
+from Anxiety.chat_anx import get_response_anx
+from det_que_or_continuation import gmh_questions, anx_questions
 from bson import ObjectId
 
 app = Flask(__name__)
@@ -140,6 +141,130 @@ def predict():
 def gt_ui():
     return render_template('gt.html')
 
+
+@app.post("/anxiety")
+def predict_anx():
+    mongo_uri = "mongodb://localhost:27017/test_mc"
+    client = pymongo.MongoClient(mongo_uri)
+    db = client.test_mc
+    collection = db.Project
+
+    target_object_id = ObjectId("65e4267c064e0a1420a3c6c5")
+
+    document = collection.find_one({"_id": target_object_id})
+    if document is None:
+        return jsonify({"error": "Document with id 1 not found"})
+    
+    user_response_arr = []
+
+    text = request.get_json().get("message")
+    val = document["val"]
+
+    response_old = document["score"]
+
+
+
+
+    if str(text).lower() == 'no':
+        message = {"output": "Okay no problem, feel free to start the test whenever you want to, I am always here for you"}
+        return jsonify(message)
+
+    elif str(text).lower() == 'yes':
+        new_data_value = val+1
+        question = anx_questions[val]
+        result = collection.update_one({"_id": target_object_id}, {"$set": {"val": new_data_value}})
+        if result.modified_count > 0:
+            message = {"output": question}
+        else:
+            message = {"output": "Data attribute not updated"}
+        return jsonify(message)
+    
+    
+    
+
+    elif val > 0 and val < 11:
+        new_data_value = val+1
+        
+        result = collection.update_one({"_id": target_object_id}, {"$set": {"val": new_data_value}})
+        res = request.get_json().get("message")
+
+
+        resp = get_response_anx(res)
+
+        resp_arr = resp.split('.')
+        resp_score = int(resp_arr[0])
+        if resp_score != 2:
+            response_old += resp_score
+
+        question = resp_arr[1]
+        question += '.Next question:' + anx_questions[val]
+        if result.modified_count > 0:
+            message = {"output": question}
+        else:
+            message = {"output": "Data attribute not updated"}
+
+        collection.update_one({"_id": target_object_id}, {"$set": {"score": response_old}})
+
+        return jsonify(message)
+    
+
+
+    elif val >=11:
+        new_data_value = 0
+        print(user_response_arr)
+        result = collection.update_one({"_id": target_object_id}, {"$set": {"val": new_data_value}})
+
+        res = request.get_json().get("message")
+
+        resp = get_response_anx(res)
+
+        resp_arr = resp.split('.')
+        resp_score = int(resp_arr[0])
+        if resp_score != 2:
+            response_old += resp_score
+
+        response_old += resp_score
+
+        collection.update_one({"_id": target_object_id}, {"$set": {"score": 0}})
+
+        precent_resp = response_old/12
+
+        score = db.score
+
+        score_data = {
+            "_idUser": ObjectId("65e4267c064e0a1420a3c6c5"),
+            "test": "Anxiety test",
+            "score": str(precent_resp*100) + '%'
+        }
+
+        score.insert_one(score_data)
+
+        if precent_resp >= 0.60 and precent_resp<0.75:
+            message = {"output": 'I would like you to listen to music and watch comedy videos as laughter is best medicine. Try to stay away from social media as it have negativity'}
+            return jsonify(message)
+        
+        elif precent_resp >= 0.75 and precent_resp < 0.9:
+            message = {"output": "Read books about mental stability. I would recommend 'An UnQuiet mind' and 'All alone with you'. I would also like you to meditate for a hour. Breating exercises and yoga are way to go!!"}
+        
+        elif precent_resp >= 0.9:
+            message = {"output": 'Have mental problems is something that everyone goes through it!! I would recommend you to visit a nearby therapist for consultancy, dont be sad as this time shall also pass. Tele manas is an Indian govt initiative for mental help, Please feel free to contact: 1-800 891 4416'}
+            return jsonify(message)
+
+        else:
+            message = {"output": 'low'}
+            return jsonify(message)
+        
+        
+
+
+    else:
+        output = get_response_anx(text)
+        message = {"output": output}
+        return jsonify(message)
+
+@app.route('/anxiety/ui')
+def anxiety_ui():
+    return render_template('anxiety.html')
 
 
 if __name__ == "__main__":
---
+++ det_que_or_continuation.py
@@ -0,0 +1,31 @@
+import random
+import os
+
+anx_questions = ['1. Have you worried a lot in the last month?',
+                 '2. Have you had headaches or other aches and pains in the last month?',
+                 '3. Have you been getting exhausted or worn out during the day or evening, even when you have not been working very hard?',
+                 '4. Lack relaxing or are you stressed?',
+                 '5. Have you been so fidgety and restless that you couldnt sit still?',
+                 '6. Do you tend to worry about your physical health?',
+                 '7. Do you often feel on stressed or mentally tense?',
+                 '8. Have there been times lately when you have been very anxious or frightened?',
+                 '9. Have you had the feeling that you have been touched un appropriately',
+                 '10. What about meeting people e.g. going into a crowded room? Making conversation?',
+                 '11. Do you avoid any of these situations (specify as appropriate) because you know you will get anxious?'
+                 ]
+
+gmh_questions = ['1. Over the past two weeks, how often have you felt down, depressed, or hopeless? (On the scale of 0 to 4)',
+                 '2. How often have you experienced significant changes in your appetite or weight? (On the scale of 0 to 4)',
+                 '3. How often have you had trouble falling asleep or staying asleep? (On the scale of 0 to 4)',
+                 '4.How often have you lost interest or pleasure in activities that you once enjoyed? (On the scale of 0 to 4)',
+                 '5. How well have you been able to concentrate on tasks or make decisions? (On the scale of 0 to 4)',
+                 '6. How often have you felt anxious, worried, or had excessive racing thoughts? (On the scale of 0 to 4)',
+                 '7. Have you felt a sense of guilt, worthlessness, or had thoughts of self-harm or suicide? (On the scale of 0 to 4)',
+                 '8. How would you rate your overall energy levels, considering both periods of agitation and times of feeling slowed down? (On the scale of 0 to 4)',
+                 '9. How well are you managing stress and daily challenges? (On the scale of 0 to 4)',
+                 '10. How satisfied are you with your current quality of life? (On the scale of 0 to 4)']
+
+
+
+        
+
